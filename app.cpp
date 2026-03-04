ce2e68723b4b49b3ea6f4a7d522891018495e019
kerollos <Koko89090k@gmail.com>
2026-03-04 21:52:56 +0200

    course

--- app.cpp
+++ app.cpp
@@ -6,27 +6,6 @@ using namespace std;
 
 int main() {
   
-  priority_queue<int, vector<int>, greater<int> > pq;
-
- int n;
- cin >> n;
-
-  long long h=0;
-
- for(int i = 0 ; i<n ; ++i){
-  int x;
-  cin >> x;
-  pq.push(x);
-  h += x;
-  
-  if(h<0){
-    h -= pq.top();
-    pq.pop();
-  }
-
- }
- 
- cout << pq.size();
 
   return 0;
 }
---
+++ course.h
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct course {
+
+string id;
+string title;
+int credit_hours;
+
+
+};
