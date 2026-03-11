bf4526134e87dd542f2bb6ac65dc259de6ee23a7
kam0lia <kamalrashad56@gnail.com>
2026-03-12 00:46:26 +0200

    modified utils.h

---
+++ utils.h
@@ -0,0 +1,11 @@
+#ifndef utils_h
+#define utils_h
+#include <iostream>
+#include <string>
+#include <vector>
+int getIntInput(const std::string& prompt);
+double getDoubleInput(const std::string& prompt);
+std::string getStringInput(const std::string& prompt);
+template <typename T>
+T findMax(const std::vector<T>& items);
+#endif
